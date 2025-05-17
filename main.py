90e7e7eda4b4282a5a321c77e42ba79d9d45b898
RafaelGitMotta <rafamotta07@gmail.com>
2025-05-17 18:23:08 -0300

    teste rail

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from estudo import app # Importa a variável app da pasta estudo no arquivo __in
 
 if __name__ == '__main__':# SE __name__ for igual a __main__(entao o pragrama executa a alinha de baixo)
     app.run(debug=True)# app.run executa a variavel app e faz a aplicação rodar.(debub=True) faz com que a cada atualização restarta a aplicação.
+
+#DEBUB_MODE = os.getenv('DEBUG_MODE')=='True'
